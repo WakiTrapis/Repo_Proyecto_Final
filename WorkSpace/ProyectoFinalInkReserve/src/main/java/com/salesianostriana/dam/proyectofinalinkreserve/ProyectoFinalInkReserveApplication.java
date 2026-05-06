b0bb1e50191ac82f4e717c85435c085755184298
WakiTrapiS-alt <mblazquez41@gmail.com>
2026-05-06 20:56:41 +0200

    Creacion del proyecto en spring

---
+++ WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/ProyectoFinalInkReserveApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectofinalinkreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalInkReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalInkReserveApplication.class, args);
+	}
+
+}
---
+++ WorkSpace/ProyectoFinalInkReserve/src/test/java/com/salesianostriana/dam/proyectofinalinkreserve/ProyectoFinalInkReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectofinalinkreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalInkReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
