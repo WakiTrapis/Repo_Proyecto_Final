962b724cc48e2e1ba3ad7c00934488c117dd4b8c
WakiTrapiS-alt <mblazquez41@gmail.com>
2026-05-08 00:06:17 +0200

    He fragmentado el nav del dashboardPrincipal y he empezado a hacer el dashboardArtistas

--- WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/controller/DashboardController.java
+++ WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/controller/DashboardController.java
@@ -8,10 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DashboardController {
 
 	@GetMapping("/Dashboard")
-	public String PintarDashboard(Model model) {
+	public String PintarDashboardPrincipal(Model model) {
 		model.addAttribute("usuario", new Object() {
 	        public String username = "Salesianos";
 	    });
 		return "DashboardPrincipal";
 	}
+	
+	@GetMapping("/Dashboard/Artistas")
+	public String PintarDashboardArtistas(Model model) {
+		model.addAttribute("usuario", new Object() {
+	        public String username = "Salesianos";
+	    });
+		return "DashboardArtistas";
+	}
+	
+	
 }
