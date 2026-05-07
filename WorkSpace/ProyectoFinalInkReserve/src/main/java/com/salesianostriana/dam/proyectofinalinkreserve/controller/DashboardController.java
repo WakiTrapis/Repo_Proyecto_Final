1a7b548f7b790069e6f6a6dc5bf49916285bbd2a
WakiTrapiS-alt <mblazquez41@gmail.com>
2026-05-07 23:35:42 +0200

    Termino el Dasboard Principal y creo un controlador para verlo en el navegador

---
+++ WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/controller/DashboardController.java
@@ -0,0 +1,17 @@
+package com.salesianostriana.dam.proyectofinalinkreserve.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+	@GetMapping("/Dashboard")
+	public String PintarDashboard(Model model) {
+		model.addAttribute("usuario", new Object() {
+	        public String username = "Salesianos";
+	    });
+		return "DashboardPrincipal";
+	}
+}
