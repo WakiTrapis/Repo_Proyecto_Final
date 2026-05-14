5b4351d140752a02d6a3ff0e0c82f9b615478a80
Miguel Angel Blazquez <blazquez.samig25@triana.salesianos.edu>
2026-05-14 13:03:41 +0200

    feat(api): Añado el controller de editar artista

--- WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/controller/ArtistaController.java
+++ WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/controller/ArtistaController.java
@@ -1,10 +1,13 @@
 package com.salesianostriana.dam.proyectofinalinkreserve.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.salesianostriana.dam.proyectofinalinkreserve.model.Artista;
 import com.salesianostriana.dam.proyectofinalinkreserve.service.ArtistaService;
@@ -31,6 +34,23 @@ public class ArtistaController {
 	@PostMapping("/nuevoArtistaCompleto")
 	public String submit(@ModelAttribute("formularioArtista") Artista artista, Model model){
 		artistaService.save(artista);
+		return "redirect:/Dashboard/Artistas";
+	}
+	
+	@GetMapping("/Dashboard/Artistas/Editar/{id}")
+	public String mostrarFormEditar(@PathVariable("id") Long id, Model model) {
+		Optional<Artista> artista = artistaService.findById(id);
+		if (artista.isPresent()) {
+			model.addAttribute("formularioArtista", artista.get());
+			return "formularioArtista";
+		} else {
+			return "redirect:/Dashboard/Artistas";
+		}	
+	}
+	
+	@PostMapping("/Dashboard/Artistas/Editar/submit")
+	public String submitEditar(@ModelAttribute("formularioArtista") Artista artista ) {
+		artistaService.edit(artista);
 		return "redirect:/Dashboard/Artistas";
 	}
 }
