17a051b506804d38a65ffa9657963daae727a1ae
Miguel Angel Blazquez <blazquez.samig25@triana.salesianos.edu>
2026-05-25 13:46:31 +0200

    fix(code): he eliminado imports inútiles y estructurado un poco mejor el codigo

--- WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/config/MvcConfig.java
+++ WorkSpace/ProyectoFinalInkReserve/src/main/java/com/salesianostriana/dam/proyectofinalinkreserve/config/MvcConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+ //Clase que determina donde guardar las imagenes de los Artistas y los Tatuajes
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
